3bc08c8f9ebfdfc12e822598bda2c0cd8a23255c
嬉皮士 <dalan_123@126.com>
2018-10-08 18:38:09 +0800

    Create cachetable.go

---
+++ cachev2.0/cachetable.go
@@ -0,0 +1,303 @@
+package cache_go
+
+import (
+	"sync"
+	"time"
+	"log"
+	"sort"
+)
+
+// cache存储空间
+type CacheTable struct {
+	sync.RWMutex
+
+	name string  // 不同的存储空间
+	items map[interface{}]*CacheItem  // items
+
+	cleanupTimer	*time.Timer  // 清理定时器
+	cleanupInterval time.Duration // 清理间隔
+
+	logger *log.Logger            // table操作记录
+
+	loadData func(key interface{}, args ...interface{}) *CacheItem  //
+	addedItem	func(item *CacheItem)
+	aboutToDeleteItem func(item *CacheItem)
+}
+
+// table中items
+func (table *CacheTable) Count() int {
+	table.RLock()
+	defer table.RUnlock()
+	return len(table.items)
+}
+
+// 遍历所有的items  通过trans方式对item进行操作
+func (table *CacheTable) Foreach(trans func(key interface{}, item *CacheItem)) {
+	table.RLock()
+	defer table.RUnlock()
+
+	for k, v := range table.items {
+		trans(k, v)
+	}
+}
+
+
+func (table *CacheTable) SetDataLoader(f func(interface{}, ...interface{}) *CacheItem) {
+	table.Lock()
+	defer table.Unlock()
+	table.loadData = f
+}
+
+
+func (table *CacheTable) SetAddedItemCallback(f func(*CacheItem)) {
+	table.Lock()
+	defer table.Unlock()
+	table.addedItem = f
+}
+
+
+func (table *CacheTable) SetAboutToDeleteItemCallback(f func(*CacheItem)) {
+	table.Lock()
+	defer table.Unlock()
+	table.aboutToDeleteItem = f
+}
+
+
+func (table *CacheTable) SetLogger(logger *log.Logger) {
+	table.Lock()
+	defer table.Unlock()
+	table.logger = logger
+}
+
+// item有效期检查 能够自动调整每次检查的周期
+func (table *CacheTable) expirationCheck() {
+	table.Lock()
+	if table.cleanupTimer != nil {
+		table.cleanupTimer.Stop()
+	}
+	if table.cleanupInterval > 0 {
+		table.log("Expiration check triggered after", table.cleanupInterval, "for table", table.name)
+	} else {
+		table.log("Expiration check installed for table", table.name)
+	}
+
+	now := time.Now()
+	smallestDuration := 0 * time.Second
+	for key, item := range table.items {
+		// Cache values so we don't keep blocking the mutex.
+		item.RLock()
+		lifeSpan := item.lifeSpan
+		accessedOn := item.accessedOn
+		item.RUnlock()
+
+		if lifeSpan == 0 {
+			continue
+		}
+		if now.Sub(accessedOn) >= lifeSpan {
+			// Item has excessed its lifespan.
+			table.deleteInternal(key)
+		} else {
+			// Find the item chronologically closest to its end-of-lifespan.
+			if smallestDuration == 0 || lifeSpan-now.Sub(accessedOn) < smallestDuration {
+				smallestDuration = lifeSpan - now.Sub(accessedOn)
+			}
+		}
+	}
+
+	// Setup the interval for the next cleanup run.
+	table.cleanupInterval = smallestDuration
+	if smallestDuration > 0 {
+		table.cleanupTimer = time.AfterFunc(smallestDuration, func() {
+			go table.expirationCheck()
+		})
+	}
+	table.Unlock()
+}
+
+// 必须对应的cachetable的lock 放开进行该操作
+func (table *CacheTable) addInternal(item *CacheItem) {
+	table.log("Adding item with key", item.key, "and lifespan of", item.lifeSpan, "to table", table.name)
+	table.items[item.key] = item
+
+	// Cache values so we don't keep blocking the mutex.
+	expDur := table.cleanupInterval
+	addedItem := table.addedItem
+	table.Unlock()
+
+	// Trigger callback after adding an item to cache.
+	if addedItem != nil {
+		addedItem(item)
+	}
+
+	// If we haven't set up any expiration check timer or found a more imminent item.
+	if item.lifeSpan > 0 && (expDur == 0 || item.lifeSpan < expDur) {
+		table.expirationCheck()
+	}
+}
+
+func (table *CacheTable) Add(key interface{}, lifeSpan time.Duration, data interface{}) *CacheItem {
+	item := NewCacheItem(key, lifeSpan, data)
+
+	// Add item to cache.
+	table.Lock()
+	table.addInternal(item)
+
+	return item
+}
+
+func (table *CacheTable) deleteInternal(key interface{}) (*CacheItem, error) {
+	r, ok := table.items[key]
+	if !ok {
+		return nil, ErrKeyNotFound
+	}
+
+	// Cache value so we don't keep blocking the mutex.
+	aboutToDeleteItem := table.aboutToDeleteItem
+	table.Unlock()
+
+	// Trigger callbacks before deleting an item from cache.
+	if aboutToDeleteItem != nil {
+		aboutToDeleteItem(r)
+	}
+
+	r.RLock()
+	defer r.RUnlock()
+	if r.aboutToExpire != nil {
+		r.aboutToExpire(key)
+	}
+
+	table.Lock()
+	table.log("Deleting item with key", key, "created on", r.createOn, "and hit", r.accessCount, "times from table", table.name)
+	delete(table.items, key)
+
+	return r, nil
+}
+
+// Delete an item from the cache.
+func (table *CacheTable) Delete(key interface{}) (*CacheItem, error) {
+	table.Lock()
+	defer table.Unlock()
+
+	return table.deleteInternal(key)
+}
+
+// 是否存在key
+func (table *CacheTable) Exists(key interface{}) bool {
+	table.RLock()
+	defer table.RUnlock()
+	_, ok := table.items[key]
+
+	return ok
+}
+
+// cache中不存在 则进行添加
+func (table *CacheTable) NotFoundAdd(key interface{}, lifeSpan time.Duration, data interface{}) bool {
+	table.Lock()
+
+	if _, ok := table.items[key]; ok {
+		table.Unlock()
+		return false
+	}
+
+	item := NewCacheItem(key, lifeSpan, data)
+	table.addInternal(item)
+
+	return true
+}
+
+
+// 获取指定key的value 同时更新access count和timestamp（调用keepalive）
+// 若是对应的key不存在cache里面通过loadData进行操作 并将对应的item添加到cache中
+func (table *CacheTable) Value(key interface{}, args ...interface{}) (*CacheItem, error) {
+	table.RLock()
+	r, ok := table.items[key]
+	loadData := table.loadData
+	table.RUnlock()
+
+	if ok {
+		// Update access counter and timestamp.
+		r.KeepAlive()
+		return r, nil
+	}
+
+	// Item doesn't exist in cache. Try and fetch it with a data-loader.
+	if loadData != nil {
+		item := loadData(key, args...)
+		if item != nil {
+			table.Add(key, item.lifeSpan, item.data)
+			return item, nil
+		}
+
+		return nil, ErrKeyNotFoundOrLoadable
+	}
+
+	return nil, ErrKeyNotFound
+}
+
+
+// 清空cache table中的items
+func (table *CacheTable) Flush() {
+	table.Lock()
+	defer table.Unlock()
+
+	table.log("Flushing table", table.name)
+
+	// 重置items map、cleanupInterval、cleanupTimer
+	table.items = make(map[interface{}]*CacheItem)
+	table.cleanupInterval = 0
+	if table.cleanupTimer != nil {
+		table.cleanupTimer.Stop()
+	}
+}
+
+//
+type CacheItemPair struct {
+	Key         interface{}
+	AccessCount int64
+}
+
+
+type CacheItemPairList []CacheItemPair
+
+func (p CacheItemPairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p CacheItemPairList) Len() int           { return len(p) }
+func (p CacheItemPairList) Less(i, j int) bool { return p[i].AccessCount > p[j].AccessCount }
+
+
+func (table *CacheTable) MostAccessed(count int64) []*CacheItem {
+	table.RLock()
+	defer table.RUnlock()
+
+	p := make(CacheItemPairList, len(table.items))
+	i := 0
+	for k, v := range table.items {
+		p[i] = CacheItemPair{k, v.accessCount}
+		i++
+	}
+	sort.Sort(p)
+
+	var r []*CacheItem
+	c := int64(0)
+	for _, v := range p {
+		if c >= count {
+			break
+		}
+
+		item, ok := table.items[v.Key]
+		if ok {
+			r = append(r, item)
+		}
+		c++
+	}
+
+	return r
+}
+
+// 操作记录.
+func (table *CacheTable) log(v ...interface{}) {
+	if table.logger == nil {
+		return
+	}
+
+	table.logger.Println(v)
+}
